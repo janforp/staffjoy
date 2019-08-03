039c6120546f841dd9b7e744d306b76e58327628
zhucj's <janforp@163.com>
2019-08-03 16:37:32 +0800

    remove

--- company-svc/src/main/java/xyz/staffjoy/company/controller/DirectoryController.java
+++ company-svc/src/main/java/xyz/staffjoy/company/controller/DirectoryController.java
@@ -14,6 +14,7 @@ import xyz.staffjoy.company.service.PermissionService;
 @RequestMapping("/v1/company/directory")
 @Validated
 public class DirectoryController {
+
     @Autowired
     DirectoryService directoryService;
 
