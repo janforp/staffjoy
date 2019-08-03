2fbd51c17bebd19734d162590dcb40a31ea1c415
zhucj <janforp@163.com>
2019-08-03 18:45:04 +0800

    remove

--- faraday/src/main/java/xyz/staffjoy/faraday/core/http/ReverseProxyFilter.java
+++ faraday/src/main/java/xyz/staffjoy/faraday/core/http/ReverseProxyFilter.java
@@ -14,7 +14,6 @@ import xyz.staffjoy.faraday.core.trace.ProxyingTraceInterceptor;
 import xyz.staffjoy.faraday.exceptions.FaradayException;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
