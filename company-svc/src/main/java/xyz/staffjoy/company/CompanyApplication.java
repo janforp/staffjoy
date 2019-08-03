26fe894a009c47afd8ae9279c2eeec6dba43d995
Administrator <janforp@163.com>
2019-08-03 16:35:13 +0800

    remove

--- company-svc/src/main/java/xyz/staffjoy/company/CompanyApplication.java
+++ company-svc/src/main/java/xyz/staffjoy/company/CompanyApplication.java
@@ -3,7 +3,6 @@ package xyz.staffjoy.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients(basePackages = {"xyz.staffjoy.account", "xyz.staffjoy.bot"})
 @SpringBootApplication
